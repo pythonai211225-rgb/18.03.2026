1f8503202fc7a4af079d93c298a7f7d375d643df
fullstack290623 <fullstack290623@gmail.com>
2026-03-18 21:21:03 +0200

    upload

--- 02_targil.py
+++ 02_targil.py
@@ -9,4 +9,12 @@ print how many times the biggest digit appears? -- 2
 print the min digit -- 4
 print the sum of digits -- 38
 print the avg of digits -- 8+8+9+4+9 / 5
+
+2
+write a function that gets a list of numbers and returns if the list is sorted or not
+
+3
+write a function that gets a list of numbers (with duplication), n-th biggest and returns it
+i.e. [88, 90, 95, 95, 97, 97, 97, 99, 100] , 4 --> will return 95 (because it is the 4-th biggest after 100, 99, 97, 95)
+
 '''
